f9ca46664cefe01b9716539887b38f1a684478b9
PaHuHo <hoang.phan804@gmail.com>
2021-11-02 14:23:50 +0700

    Giao dien trang doi sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 void main() {
-  // runApp(const MyApp());
+  // runApp(const MaterialApp(
+  //   title: 'Navigation Basics',
+  //   home: WaitSignIn(),
+  // ));
 }
 
+class WaitSignIn extends StatelessWidget {
+  const WaitSignIn({Key? key}) : super(key: key);
 
-
-
-
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xff02bbd2),
+        body: Center(
+            child: Column(children: [
+          Expanded(child: Column(children:[Image.asset('images/Logo.PNG'),
+          Text('SignIn',style: TextStyle(fontSize: 60,fontWeight: FontWeight.w400,color: Colors.white)),
+          Text('Speak, friend, and enter',style: TextStyle(fontWeight: FontWeight.w300))])),   
+          Container(padding:EdgeInsets.only(bottom: 150),child:SpinKitFadingCircle(
+            color: Colors.white,
+            size: 50.0,
+          )),
+        ])));
+  }
+}
