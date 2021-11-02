630052e867250dc21278c31234983f2c215304dd
NguyenMinhDuc210701 <83911041+NguyenMinhDuc210701@users.noreply.github.com>
2021-11-02 16:18:48 +0700

    Giao dien trang SignIn

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,100 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // runApp(const MyApp());
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: MainSignIn(),
+  ));
 }
+class MainSignIn extends StatelessWidget{
+  const MainSignIn ({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context){
+    Widget imgSection = Image.asset('images/Logo.jpg',
+      alignment: Alignment.center,
+      width: 150,
+      height: 150,
+    );
+    Widget TextSection = Container(
+      child: Column(
+        children: [
+          Container(
+            child: Text('SignIn',
+              style: TextStyle(fontSize: 50, color: Colors.white),
+            ),
+          ),
+          Text('Speak, friend, and enter'),
+        ],
+      ),
+    );
+    Widget LoginSection = Container(
+      child: Column(
+        
+        children: [
+          Container(
+            width: 450,
+            height: 50,
+            margin: EdgeInsets.only(bottom: 10),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Email',
+                fillColor: Colors.white,
+                filled: true,
+              ),
+              textAlign: TextAlign.center,
+              textAlignVertical: TextAlignVertical.center,
+            ),
+          ),
+          Container(
+            width: 450,
+            height: 50,
+            child: TextField(
+              obscureText: true, 
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Password',
+                fillColor: Colors.white,
+                filled: true,
+                ),
+                textAlign: TextAlign.center,
+                textAlignVertical: TextAlignVertical.center,
+              ),
+            ),
+        ],
+      ),
+    );
+    Widget SignInButton = Container(
+      width: 450,
+      height: 50,
+      child: TextButton(
+        child: Text('SIGN IN',
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: (){},
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+        ),
+      ),
+    );
+    
+    return Scaffold(
+      backgroundColor: Color(0xff02bbd2),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+          imgSection,
+          TextSection,
+          LoginSection,
+          SignInButton,
+          ],
+        ),   
+      ),
+    );
+  } 
+}
 
 
 
