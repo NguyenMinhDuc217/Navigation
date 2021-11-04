2d2b5b8be797436265bc7b775df87cef91368bb9
PaHuHo <hoang.phan804@gmail.com>
2021-11-05 06:49:42 +0700

    Xong chuc nang signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 void main() {
   runApp(const MaterialApp(
     title: 'Navigation Basics',
-    home: MainSignIn(),
+    home: MyApp(),
   ));
 }
-class MainSignIn extends StatelessWidget{
-  const MainSignIn ({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    Widget _mainSignIn=MainSignIn();
+    return _mainSignIn;
+  }
+  
+}
+class MainSignIn extends StatefulWidget{
+  @override
+  MainSignInState createState() => MainSignInState();
+}
+class MainSignInState extends State<MainSignIn> {
+  final _controller1=TextEditingController();
+  final _controller2=TextEditingController();
 
   @override
-  Widget build(BuildContext context){
-    Widget imgSection = Image.asset('images/Logo.jpg',
+  Widget build(BuildContext context) {
+    Widget imgSection = Image.asset(
+      'images/Logo.jpg',
       alignment: Alignment.center,
       width: 150,
       height: 150,
@@ -21,7 +38,8 @@ class MainSignIn extends StatelessWidget{
       child: Column(
         children: [
           Container(
-            child: Text('SignIn',
+            child: Text(
+              'SignIn',
               style: TextStyle(fontSize: 50, color: Colors.white),
             ),
           ),
@@ -31,13 +49,13 @@ class MainSignIn extends StatelessWidget{
     );
     Widget LoginSection = Container(
       child: Column(
-        
         children: [
           Container(
             width: 450,
             height: 50,
             margin: EdgeInsets.only(bottom: 10),
             child: TextField(
+              controller: _controller1,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 hintText: 'Email',
@@ -52,17 +70,18 @@ class MainSignIn extends StatelessWidget{
             width: 450,
             height: 50,
             child: TextField(
-              obscureText: true, 
+              controller: _controller2,
+              obscureText: true,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 hintText: 'Password',
                 fillColor: Colors.white,
                 filled: true,
-                ),
-                textAlign: TextAlign.center,
-                textAlignVertical: TextAlignVertical.center,
               ),
+              textAlign: TextAlign.center,
+              textAlignVertical: TextAlignVertical.center,
             ),
+          ),
         ],
       ),
     );
@@ -70,60 +89,87 @@ class MainSignIn extends StatelessWidget{
       width: 450,
       height: 50,
       child: TextButton(
-        child: Text('SIGN IN',
+        child: Text(
+          'SIGN IN',
           style: TextStyle(color: Colors.white),
         ),
-        onPressed: (){},
+        onPressed: () {
+          if(_controller1.text==_controller2.text){
+              Navigator.push(
+              context, MaterialPageRoute(builder: (context) => WaitSignIn()));
+          }
+          
+        },
         style: ButtonStyle(
           backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
         ),
       ),
     );
-    
+
     return Scaffold(
       backgroundColor: Color(0xff02bbd2),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-          imgSection,
-          TextSection,
-          LoginSection,
-          SignInButton,
+            imgSection,
+            TextSection,
+            LoginSection,
+            SignInButton,
           ],
-        ),   
+        ),
       ),
     );
-  } 
+  }
 }
-class WaitSignIn extends StatelessWidget {
-  const WaitSignIn({Key? key}) : super(key: key);
+
+class WaitSignIn extends StatefulWidget {
+  @override
+  WaitSignInState createState() => WaitSignInState();
+}
+
+class WaitSignInState extends State<WaitSignIn> {
+  @override
+  void initState() {
+    //Dem nguoc 3s roi mo trang main signin
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => EmailPage()));
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color(0xff02bbd2),
         body: Center(
-            child: Column(
-                children: [
-              Image.asset('images/Logo.PNG'),
-              Column(
-                children: [
-                  Text('SignIn',
-                      style: TextStyle(
-                          fontSize: 50,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white)),
-                  Text('Speak, friend, and enter',
-                      style: TextStyle(fontWeight: FontWeight.w300)),
-                ],
-              ),
-              Container(
-                  margin: EdgeInsets.only(top: 150),
-                  child: SpinKitFadingCircle(
-                    color: Colors.white,
-                    size: 50.0,
-                  )),
-            ])));
+            child: Column(children: [
+          Image.asset('images/Logo.PNG'),
+          Column(
+            children: [
+              Text('SignIn',
+                  style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white)),
+              Text('Speak, friend, and enter',
+                  style: TextStyle(fontWeight: FontWeight.w300)),
+            ],
+          ),
+          Container(
+              margin: EdgeInsets.only(top: 150),
+              child: SpinKitFadingCircle(
+                color: Colors.white,
+                size: 50.0,
+              )),
+        ])));
+  }
+}
+
+class EmailPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(body: Text('Email'));
   }
 }
